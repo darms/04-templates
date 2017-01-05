77eab1a69c9100641d44b8dbe4968e1c293af4b7
DARCY MCCABE <DARMCCA@GMAIL.COM>
2017-01-05 13:05:04 -0800

    Finished TODO 3: Grabbed from the DOM and compiled info with handlebars method

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,10 +12,11 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles.
+  // TODO:DONE Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#neighborhood-template').html();
+  var templateRender = Handlebars.compile(source);
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
